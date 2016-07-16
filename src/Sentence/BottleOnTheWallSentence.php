4c1a0cb57971ffd5d6dce9cb13bbb021ac435a82
Luis Rovirosa <luisrovirosa@gmail.com>
2016-07-16 19:35:42 +0200

    BottleOnTheWallSentence the_last_sentence_is_no_more_bottles

--- src/Sentence/BottleOnTheWallSentence.php
+++ src/Sentence/BottleOnTheWallSentence.php
@@ -17,7 +17,18 @@ class BottleOnTheWallSentence extends Sentence {
    * @return string
    */
   private function sentence($numberOfSentence) {
+    $bottlesOnTheWall = $this->bottlesOnTheWall($numberOfSentence);
+
+    return ucfirst($bottlesOnTheWall) . " bottles of beer on the wall, $bottlesOnTheWall bottles of beer.";
+  }
+
+  /**
+   * @param $numberOfSentence
+   * @return int
+   */
+  private function bottlesOnTheWall($numberOfSentence) {
     $bottlesOnTheWall = self::MAX_NUMBER_OF_BOTTLES_ON_THE_WALL - $numberOfSentence + 1;
-    return "$bottlesOnTheWall bottles of beer on the wall, $bottlesOnTheWall bottles of beer.";
+
+    return $bottlesOnTheWall ? $bottlesOnTheWall : "no more";
   }
 }
---
+++ tests/Sentence/BottleOnTheWallSentenceTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Song99Bottles\Tests\Sentence;
+
+use Song99Bottles\Sentence\BottleOnTheWallSentence;
+use Song99Bottles\Singer;
+
+class BottleOnTheWallSentenceTest extends \PHPUnit_Framework_TestCase {
+  /** @test */
+  public function the_last_sentence_is_no_more_bottles() {
+    $sentence = new BottleOnTheWallSentence(100);
+    $singer = $this->prophesize(Singer::class);
+
+    $sentence->sing($singer->reveal());
+
+    $singer->sing('No more bottles of beer on the wall, no more bottles of beer.')->shouldHaveBeenCalled();
+  }
+}
