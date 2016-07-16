c142b72d74aee3f6f7e37211048a52138a0b66ec
Luis Rovirosa <luisrovirosa@gmail.com>
2016-07-16 16:23:17 +0200

    refactor: extract constant

--- src/Lyric99Bottles.php
+++ src/Lyric99Bottles.php
@@ -3,12 +3,13 @@
 namespace Song99Bottles;
 
 class Lyric99Bottles {
+  const NUMBER_OF_STANZAS = 100;
 
   /**
    * @return Stanza[]
    */
   public function stanzas() {
-    $numberOfBottles = range(1, 100);
+    $numberOfBottles = range(1, self::NUMBER_OF_STANZAS);
 
     return array_map(function ($numberOfBottles) {
       return Stanza::sentence($numberOfBottles);
