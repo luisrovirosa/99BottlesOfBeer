70ba9233a0fe096ac1a2fc44a3d7b337d19fb64a
Luis Rovirosa <luisrovirosa@gmail.com>
2016-07-16 19:49:46 +0200

    BottleOnTheWallSentence the_first_sentence_is_99_bottles

--- tests/Sentence/BottleOnTheWallSentenceTest.php
+++ tests/Sentence/BottleOnTheWallSentenceTest.php
@@ -7,6 +7,16 @@ use Song99Bottles\Singer;
 
 class BottleOnTheWallSentenceTest extends \PHPUnit_Framework_TestCase {
   /** @test */
+  public function the_first_sentence_is_99_bottles() {
+    $sentence = new BottleOnTheWallSentence(1);
+    $singer = $this->prophesize(Singer::class);
+
+    $sentence->sing($singer->reveal());
+
+    $singer->sing('99 bottles of beer on the wall, 99 bottles of beer.')->shouldHaveBeenCalled();
+  }
+
+  /** @test */
   public function the_last_sentence_is_no_more_bottles() {
     $sentence = new BottleOnTheWallSentence(100);
     $singer = $this->prophesize(Singer::class);
