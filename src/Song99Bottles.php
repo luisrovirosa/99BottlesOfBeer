abb4d9e8adac651f47c969d375ff1c1e76d7353e
Luis Rovirosa <luisrovirosa@gmail.com>
2016-07-16 11:37:59 +0200

    refactor: extract method

--- src/Song99Bottles.php
+++ src/Song99Bottles.php
@@ -17,10 +17,25 @@ class Song99Bottles {
   }
 
   public function sing() {
+    $sentences = $this->sentences();
+    $this->singSentences($sentences);
+  }
+
+  /**
+   * @return array
+   */
+  private function sentences() {
     $sentences = [
       '99 bottles of beer on the wall, 99 bottles of beer.',
       'Take one down and pass it around, 98 bottles of beer on the wall.'
     ];
+    return $sentences;
+  }
+
+  /**
+   * @param $sentences
+   */
+  private function singSentences($sentences) {
     array_map(function ($sentence) {
       $this->output->sing($sentence);
     }, $sentences);
