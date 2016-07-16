3df17009160d10c50d380eb2e4aea8863e4d0472
Luis Rovirosa <luisrovirosa@gmail.com>
2016-07-16 12:42:05 +0200

    Lyric99Bottles should_have_100_stanzas

--- src/Lyric99Bottles.php
+++ src/Lyric99Bottles.php
@@ -5,10 +5,13 @@ namespace Song99Bottles;
 class Lyric99Bottles {
 
   public function stanzas() {
-    $stanza = new Stanza([
-      new Sentence('99 bottles of beer on the wall, 99 bottles of beer.'),
-      new Sentence('Take one down and pass it around, 98 bottles of beer on the wall.')
-    ]);
-    return [$stanza];
+    $stanzas = [];
+    for ($i = 0; $i < 100; $i++) {
+      $stanzas[] = new Stanza([
+        new Sentence('99 bottles of beer on the wall, 99 bottles of beer.'),
+        new Sentence('Take one down and pass it around, 98 bottles of beer on the wall.')
+      ]);
+    }
+    return $stanzas;
   }
 }
---
+++ tests/Lyric99BottlesTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Song99Bottles\Tests;
+
+use Song99Bottles\Lyric99Bottles;
+
+class Lyric99BottlesTest extends \PHPUnit_Framework_TestCase{
+  /** @test */
+  public function should_have_100_stanzas() {
+    $lyric = new Lyric99Bottles();
+
+    $stanzas = $lyric->stanzas();
+
+    $this->assertCount(100, $stanzas);
+  }
+}
