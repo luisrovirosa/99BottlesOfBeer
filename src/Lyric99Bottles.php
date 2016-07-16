ec6cadae75bb97fda46fbcabf5b525896eb41a42
Luis Rovirosa <luisrovirosa@gmail.com>
2016-07-16 12:50:56 +0200

    Lyric99Bottles sing_second_stanza_sings_98_bottles

--- src/Lyric99Bottles.php
+++ src/Lyric99Bottles.php
@@ -4,12 +4,16 @@ namespace Song99Bottles;
 
 class Lyric99Bottles {
 
+  /**
+   * @return Stanza[]
+   */
   public function stanzas() {
     $stanzas = [];
     for ($i = 0; $i < 100; $i++) {
+      $remainingBottles = 99 - $i;
       $stanzas[] = new Stanza([
-        new Sentence('99 bottles of beer on the wall, 99 bottles of beer.'),
-        new Sentence('Take one down and pass it around, 98 bottles of beer on the wall.')
+        new Sentence("$remainingBottles bottles of beer on the wall, $remainingBottles bottles of beer."),
+        new Sentence("Take one down and pass it around, " . ($remainingBottles - 1) . " bottles of beer on the wall.")
       ]);
     }
     return $stanzas;
--- tests/Lyric99BottlesTest.php
+++ tests/Lyric99BottlesTest.php
@@ -3,8 +3,10 @@
 namespace Song99Bottles\Tests;
 
 use Song99Bottles\Lyric99Bottles;
+use Song99Bottles\Output;
+use Song99Bottles\Singer;
 
-class Lyric99BottlesTest extends \PHPUnit_Framework_TestCase{
+class Lyric99BottlesTest extends \PHPUnit_Framework_TestCase {
   /** @test */
   public function should_have_100_stanzas() {
     $lyric = new Lyric99Bottles();
@@ -12,5 +14,17 @@ class Lyric99BottlesTest extends \PHPUnit_Framework_TestCase{
     $stanzas = $lyric->stanzas();
 
     $this->assertCount(100, $stanzas);
+  }
+
+  /** @test */
+  public function sing_second_stanza_sings_98_bottles() {
+    $lyric = new Lyric99Bottles();
+    $stanzas = $lyric->stanzas();
+    $stanza = $stanzas[1];
+    $output = $this->prophesize(Output::class);
+
+    $stanza->sing(new Singer($output->reveal()));
+
+    $output->sing("98 bottles of beer on the wall, 98 bottles of beer.")->shouldHaveBeenCalled();
   }
 }
