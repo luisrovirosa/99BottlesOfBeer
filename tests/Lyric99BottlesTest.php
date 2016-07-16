d57d6e56110e3d8842fb089161ce9b606eca6215
Luis Rovirosa <luisrovirosa@gmail.com>
2016-07-16 16:01:56 +0200

    refactor: moved tests from Lyric99Bottles to Stanza

--- tests/Lyric99BottlesTest.php
+++ tests/Lyric99BottlesTest.php
@@ -2,11 +2,7 @@
 
 namespace Song99Bottles\Tests;
 
-use Prophecy\Prophecy\ObjectProphecy;
 use Song99Bottles\Lyric99Bottles;
-use Song99Bottles\Output;
-use Song99Bottles\Singer;
-use Song99Bottles\Stanza;
 
 class Lyric99BottlesTest extends \PHPUnit_Framework_TestCase {
   /** @test */
@@ -17,49 +13,5 @@ class Lyric99BottlesTest extends \PHPUnit_Framework_TestCase {
 
     $this->assertCount(100, $stanzas);
   }
-
-  /** @test */
-  public function sing_second_stanza_sings_98_bottles() {
-    $output = $this->output();
-
-    $this->sing($this->stanza(), $output);
-
-    $output->sing("98 bottles of beer on the wall, 98 bottles of beer.")->shouldHaveBeenCalled();
-  }
-
-  /** @test */
-  public function sing_second_stanza_sings_takes_one() {
-    $output = $this->output();
-
-    $this->sing($this->stanza(), $output);
-
-    $output->sing("Take one down and pass it around, 97 bottles of beer on the wall.")->shouldHaveBeenCalled();
-  }
-
-  /**
-   * @return \Song99Bottles\Stanza
-   */
-  private function stanza() {
-    $lyric = new Lyric99Bottles();
-    $stanzas = $lyric->stanzas();
-
-    return $stanzas[1];
-  }
-
-  /**
-   * @param Stanza $stanza
-   * @param ObjectProphecy $objectProphecy
-   */
-  private function sing(Stanza $stanza, ObjectProphecy $objectProphecy) {
-    /** @var Output $output */
-    $output = $objectProphecy->reveal();
-    $stanza->sing(new Singer($output));
-  }
-
-  /**
-   * @return \Prophecy\Prophecy\ObjectProphecy|\Song99Bottles\Output
-   */
-  private function output() {
-    return $this->prophesize(Output::class);
-  }
+  
 }
--- tests/StanzaTest.php
+++ tests/StanzaTest.php
@@ -2,10 +2,47 @@
 
 namespace Song99Bottles\Tests;
 
+use Prophecy\Prophecy\ObjectProphecy;
+use Song99Bottles\Output;
 use Song99Bottles\Singer;
 use Song99Bottles\Stanza;
 
 class StanzaTest extends \PHPUnit_Framework_TestCase {
+
+  /** @test */
+  public function sing_second_stanza_sings_98_bottles() {
+    $output = $this->output();
+
+    $this->sing(Stanza::forBottles(98), $output);
+
+    $output->sing("98 bottles of beer on the wall, 98 bottles of beer.")->shouldHaveBeenCalled();
+  }
+
+  /** @test */
+  public function sing_second_stanza_sings_takes_one() {
+    $output = $this->output();
+
+    $this->sing(Stanza::forBottles(98), $output);
+
+    $output->sing("Take one down and pass it around, 97 bottles of beer on the wall.")->shouldHaveBeenCalled();
+  }
+
+  /**
+   * @param Stanza $stanza
+   * @param ObjectProphecy $objectProphecy
+   */
+  private function sing(Stanza $stanza, ObjectProphecy $objectProphecy) {
+    /** @var Output $output */
+    $output = $objectProphecy->reveal();
+    $stanza->sing(new Singer($output));
+  }
+
+  /**
+   * @return \Prophecy\Prophecy\ObjectProphecy|\Song99Bottles\Output
+   */
+  private function output() {
+    return $this->prophesize(Output::class);
+  }
 
   /** @test */
   public function singer_should_make_a_pause_sing_a_stanza() {
