1f922b77dda8a00e1646309eb097dd227e19e32f
Luis Rovirosa <luisrovirosa@gmail.com>
2016-07-16 13:00:27 +0200

    refactor: rename and extract variable

--- src/Lyric99Bottles.php
+++ src/Lyric99Bottles.php
@@ -10,10 +10,11 @@ class Lyric99Bottles {
   public function stanzas() {
     $stanzas = [];
     for ($i = 0; $i < 100; $i++) {
-      $remainingBottles = 99 - $i;
+      $bottlesOnTheWall = 99 - $i;
+      $bottlesAfterTakeOne = $bottlesOnTheWall - 1;
       $stanzas[] = new Stanza([
-        new Sentence("$remainingBottles bottles of beer on the wall, $remainingBottles bottles of beer."),
-        new Sentence("Take one down and pass it around, " . ($remainingBottles - 1) . " bottles of beer on the wall.")
+        new Sentence("$bottlesOnTheWall bottles of beer on the wall, $bottlesOnTheWall bottles of beer."),
+        new Sentence("Take one down and pass it around, " . $bottlesAfterTakeOne . " bottles of beer on the wall.")
       ]);
     }
     return $stanzas;
